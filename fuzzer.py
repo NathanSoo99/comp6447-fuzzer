34a4885681a1a02a16867a2a924d9c3ab2f495d6
Nathan <nathansoo99@gmail.com>
2025-11-14 15:22:26 +1100

    added several byte flip strategies

--- fuzzer.py
+++ fuzzer.py
@@ -1,109 +1,24 @@
-import subprocess
 import os
 
-BINARIES_DIR_PATH = "./binaries"
-EXAMPLE_INPUTS_DIR_PATH = "./example_inputs"
-TEMP_INPUTS_DIR_PATH = "./temp_inputs"
-FUZZER_OUTPUT_DIR_PATH = "./fuzzer_output"
+from fuzzes.flips import single_byte_flip_char, single_byte_flip_csv, single_byte_flip_json, single_byte_flip_xml
+from fuzzes.flips import single_byte_flip_null, single_byte_flip_ff, single_byte_remove
+from fuzzes.basics import input_nothing, duplicate_input, long_lines_append_end
+from fuzzes.files import BINARIES_DIR_PATH, EXAMPLE_INPUTS_DIR_PATH, TEMP_INPUTS_DIR_PATH, FUZZER_OUTPUT_DIR_PATH
 
 is_checkin = True
 checkin_binaries = ["csv1", "json1"]
 
-def write_output(binary_name, output):
-    output_file = open(f"{FUZZER_OUTPUT_DIR_PATH}/{binary_name}.txt", "w")
-    output_file.seek(0)
-    output_file.write(output.decode("utf-8"))
-    output_file.truncate()
-    output_file.close()
-
-
-# input empty file
-def input_nothing(temp_input_file, example_input, binary_name):
-    temp_input_file.seek(0)
-    temp_input_file.truncate()
-
-    print("Test - input nothing")
-    result = subprocess.run([f"./binaries/{binary_name}"], stdin=temp_input_file)
-
-    if result.returncode != 0:
-        print(f"Program crash triggered with no input")
-        write_output(binary_name, "")
-        return True
-
-    print("Program exited normally")
-    return False
-
-# input duplicated example input
-def duplicate_input(temp_input_file, example_input, binary_name):
-    temp_input_file.seek(0)
-    temp_input_file.truncate()
-
-    test_input = b""
-    print("Test - duplicate example input")
-
-    for i in range(0, 10000):
-        test_input += example_input
-
-    temp_input_file.write(test_input)
-
-    temp_input_file.seek(0)
-
-    result = subprocess.run([f"./binaries/{binary_name}"], stdin=temp_input_file)
-
-    if result.returncode != 0:
-        print(f"Program crash triggered with input duplicated 1000000")
-        write_output(binary_name, test_input)
-        return True
-
-    print("Program exited normally with input duplicated 1000000")
-    return False
-
-def long_lines_append_end(temp_input_file, example_input, binary_name):
-    temp_input_file.seek(0)
-    temp_input_file.truncate()
-
-    print("Test - many long lines made by appending character to end")
-
-    i = 0
-    while i < len(example_input) and example_input[i] != 10:
-        i += 1
-
-    example_line = example_input[0:i]
-
-    temp_input_file.write(example_line)
-    temp_input_file.write(b"\n")
-
-    example_line += example_line * 100 + b"\n"
-
-    i = 0
-    while i < 1000:
-        temp_input_file.write(example_line)
-        i += 1
-
-    temp_input_file.seek(0)
-
-    result = subprocess.run([f"./binaries/{binary_name}"], stdin=temp_input_file)
-    
-    if result.returncode != 0:
-        print(f"Program crash triggered with 1000 lines of minimal 1000 characters")
-        write_output(binary_name, example_line)
-        return True
-    
-    print("Program exited normally with 1000 lines of minimal 1000 characters")
-
-    return False
-
-# single byte flip
-def single_byte_flip(temp_input_file, example_input, binary_name):
-    temp_input_file.seek(0)
-    temp_input_file.truncate()
-
-    test_input = example_input.copy()
-
 fuzz_tests = [
-    input_nothing,
-    duplicate_input,
-    long_lines_append_end
+    #input_nothing,
+    #duplicate_input,
+    #long_lines_append_end,
+    #single_byte_flip_char,
+    #single_byte_flip_null,
+    #single_byte_flip_ff,
+    single_byte_remove,
+    #single_byte_flip_csv,
+    #single_byte_flip_json,
+    #single_byte_flip_xml,
 ]
 
 def fuzz_binary(binary_name):
---
+++ fuzzes/files.py
@@ -0,0 +1,21 @@
+BINARIES_DIR_PATH = "./binaries"
+EXAMPLE_INPUTS_DIR_PATH = "./example_inputs"
+TEMP_INPUTS_DIR_PATH = "./temp_inputs"
+FUZZER_OUTPUT_DIR_PATH = "./fuzzer_output"
+
+def write_output(binary_name, output):
+    output_file = open(f"{FUZZER_OUTPUT_DIR_PATH}/{binary_name}.txt", "w")
+    output_file.seek(0)
+    output_file.write(output.decode("utf-8"))
+    output_file.truncate()
+    output_file.close()
+
+def clear_file(file):
+    file.seek(0)
+    file.truncate()
+
+def overwrite_file(file, data):
+    file.seek(0)
+    file.write(data)
+    file.truncate()
+    file.seek(0)
