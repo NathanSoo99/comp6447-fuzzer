5b3d6001aefb3f57861f9845212bbd382897ee9a
arcueun <arcueun@gmail.com>
2025-11-19 23:40:43 +1100

    init harness.py

---
+++ harness.py
@@ -0,0 +1,45 @@
+#!/bin/sh 
+import subprocess
+from pwn import *
+
+class Harness:
+    def __init__(self):
+        pass
+
+    def runBinary(self, binary, input):
+        """run the binary and return outcomes of running the program"""
+        # can we use pwntools for this? 
+        process = binary
+        process = subprocess.Popen(
+            [binary],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        stdout, stderr = process.communicate(input=input)
+        return stdout, stderr, process.returncode
+
+    def startLogger(self, inputName):
+        """reset all measurable variables to ensure fair observation for binary exploit"""
+        self.timeStart = time.perf_counter() # records total time at endLogger
+        # self.result =  
+        self.inputUsed = inputName
+
+    def getSummary(self, retCode):
+        """get a summary of the run"""
+        timeEnd = time.perf_counter() - self.timeStart
+        summary = {
+            "input": self.inputUsed,
+            "return_code": retCode,
+            "time_taken": totalTime,
+        }
+        return summary
+        # maybe write this to stdout/a file 
+
+
+    def run(self, command):
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        return result.stdout, result.stderr, result.returncode
+
+    
+
