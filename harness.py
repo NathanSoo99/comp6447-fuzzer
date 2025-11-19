1a313320591ce84282b7b4b41045af5010ef9824
arcueun <arcueun@gmail.com>
2025-11-19 23:51:12 +1100

    remove unhelpful comment

--- harness.py
+++ harness.py
@@ -6,6 +6,13 @@ class Harness:
     def __init__(self):
         pass
 
+    def startLogger(self, inputName):
+        """stats collection and display initiation"""
+        self.timeStart = time.perf_counter() # records total time at endLogger
+        # self.result =  
+        self.inputUsed = inputName
+        # maybe add more things to be interested about
+    
     def runBinary(self, binary, input):
         """run the binary and return outcomes of running the program w/ input"""
         # can we use pwntools for this? i'm not really sure how this part would work
@@ -18,16 +25,9 @@ class Harness:
         )
         stdout, stderr = process.communicate(input=input)
         return stdout, stderr, process.returncode
-
-    def startLogger(self, inputName):
-        """reset measurable variables to observation for binary exploit"""
-        self.timeStart = time.perf_counter() # records total time at endLogger
-        # self.result =  
-        self.inputUsed = inputName
-        # maybe add more things to be interested about 
-
+ 
     def getSummary(self, retCode):
-        """get a summary of the run"""
+        """calculate stats and etc after running bin"""
         timeEnd = time.perf_counter() - self.timeStart
         summary = {
             "input": self.inputUsed,
@@ -35,7 +35,12 @@ class Harness:
             "time_taken": totalTime,
         }
         return summary
-        # maybe write this to stdout/a file 
+        # don't write this to afile
+
+    def getCoverage(self):
+        """using ptrace find coverage of binary"""
+        return 'todo!' #TODO
+
 
     def run(self, command):
         # idk this was co-pilot generated kido f random bolierplate 
