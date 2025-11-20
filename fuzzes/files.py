4d227373b016343b49aaa8a347e9e64abdae7824
Joshua Lai <zafkielp.39@gmail.com>
2025-11-20 23:22:42 +1100

    Update to output file names to match spec

--- fuzzes/files.py
+++ fuzzes/files.py
@@ -3,7 +3,7 @@ EXAMPLE_INPUTS_DIR_PATH = "./example_inputs"
 FUZZER_OUTPUT_DIR_PATH = "./fuzzer_output"
 
 def write_output(binary_name, output):
-    output_file = open(f"{FUZZER_OUTPUT_DIR_PATH}/{binary_name}.txt", "w")
+    output_file = open(f"{FUZZER_OUTPUT_DIR_PATH}/bad_{binary_name}.txt", "w")
     output_file.seek(0)
     output_file.write(output.decode("utf-8"))
     output_file.truncate()
